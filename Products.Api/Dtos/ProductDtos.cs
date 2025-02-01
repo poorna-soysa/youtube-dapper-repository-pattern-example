b21bcb48f1302d11d3fa71aee9fa84ecfc02cf75
Poorna Soysa <poornapiumal.soysa@gmail.com>
2025-02-02 00:35:04 +0530

    Changed

--- Products.Api/Dtos/ProductDtos.cs
+++ Products.Api/Dtos/ProductDtos.cs
@@ -1,4 +1,5 @@
 ﻿namespace Products.Api.Dtos;
 
 public sealed record CreateProductRequest(string Name, decimal Price, int Stock);
+
 public sealed record UpdateProductRequest(Guid Id, string Name, decimal Price, int Stock);
